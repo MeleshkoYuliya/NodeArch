554aead773184c0af73e0e17d53f541819b11b21
MeleshkoYuliya <Yuliya.Meleshko@ventionteams.com>
2024-09-29 18:23:04 +0300

    Create form

--- homeWork3097/homeWork3097.js
+++ homeWork3097/homeWork3097.js
@@ -22,47 +22,132 @@ function logLineSync(logFilePath, logLine) {
   fs.closeSync(logFd);
 }
 
-function handleValidate(values) {
-  const { age, name } = values;
+const handleValidate = ({ age, name }) => {
+  let errors = {};
 
-  let errors;
-  console.log(age, name, "!!!!!");
-  if (!age || Number(age) < 18) {
+  if (isNaN(Number(age)) || Number(age) < 18) {
     errors.age = "Please enter valid age!";
   } else {
     errors.age = "";
   }
 
-  if (!name || name?.length < 3) {
+  if (name?.length < 4) {
     errors.name = "Name should be at least 4 symbols";
   } else {
     errors.name = "";
   }
-
   return errors;
-}
+};
+
+const form = `<form
+      style="display:flex; flex-direction:column; width: 400px; gap: 24px"
+      name='anketa'
+      novalidate
+      action="/send"
+      method="POST"
+    >
+      <h1>Pesonal Info</h1>
+      <input name="name" placeholder="Enter name" style="width: 400px; padding: 12px; font-size:20px; box-sizing: border-box">
+      <input name="age" placeholder="Enter age" style="width: 400px; padding: 12px; font-size:20px; box-sizing: border-box">
+      <input type="submit" value="Send" style="height: 44px; width: 400px; font-size:20px; background-color: #fff" />
+    </form>`;
 
 webserver.post("/send", (req, res) => {
-  const validationErrors = handleValidate(req.body);
-  res.send(`Data: ${JSON.stringify(req.body)}`);
+  const errors = handleValidate({ age: req.body.age, name: req.body.name });
+
+  if (errors?.age || errors?.name) {
+    res.send(`
+      <html>
+          <body>
+              <form id="redirectForm" method="post" action="/form">
+                <input type="hidden" name="data" value='${JSON.stringify({
+                  errors,
+                  age: req.body.age,
+                  name: req.body.name,
+                })}' />
+              </form>
+              <script>
+                  document.getElementById('redirectForm').submit();
+              </script>
+          </body>
+      </html>
+  `);
+  } else {
+    res.send(`
+      <html>
+          <body>
+              <form id="redirectForm" method="post" action="/thank-you">
+                <input type="hidden" name="data" value='${JSON.stringify(
+                  req.body
+                )}' />
+              </form>
+              <script>
+                  document.getElementById('redirectForm').submit();
+              </script>
+          </body>
+      </html>
+  `);
+  }
+});
+
+webserver.post("/thank-you", (req, res) => {
+  const data = JSON.parse(req.body.data);
+  res.send(
+    `Thank you for your submission! Received data: Name - ${data?.name}, Age - ${data?.age}`
+  );
 });
 
 webserver.get("/form", (req, res) => {
-  res.status(200).send(
-    `<form
-      style="display:flex; flex-direction:column; width: 400px; gap: 24px"
-      name='anketa'
-      method=post
-      action='/send'
-      novalidate
-      target="example"
-    >
-      <h1>Pesonal Info</h1>
-      <input name="name" placeholder="Enter name">
-      <input name="age" placeholder="Enter age">
-      <input type="submit" value="Send" />
-    </form>`
-  );
+  res.status(200).send(`
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <title>Anketa</title>
+      </head>
+      <body>
+        <div id="app">${form}</div>
+      </body>
+    </html>
+    `);
+});
+
+webserver.post("/form", (req, res) => {
+  const data = JSON.parse(req.body.data);
+
+  res.send(`
+        <html>
+            <body>
+                <form
+                  style="display:flex; flex-direction:column; width: 400px; gap: 24px"
+                  name='anketa'
+                  novalidate
+                  action="/send"
+                  method="POST"
+                >
+                  <h1>Pesonal Info</h1>
+                  <div>
+                    <input
+                      name="name"
+                      placeholder="Enter name"
+                      value="${data.name}"
+                      style="width: 400px; padding: 12px; font-size:20px; box-sizing: border-box"
+                    >
+                    <div style="color:red; font-size:14px">${data?.errors?.name}</div>
+                  </div>
+                  <div>
+                    <input
+                      name="age"
+                      placeholder="Enter age"
+                      value="${data.age}"
+                      style="width: 400px; padding: 12px; font-size:20px; box-sizing: border-box"
+                    >
+                    <div style="color:red; font-size:14px">${data?.errors?.age}</div>
+                  </div>
+                  <input type="submit" value="Send" style="height: 44px; width: 400px; font-size:20px; background-color: #fff" />
+                </form>
+            </body>
+        </html>
+    `);
 });
 
 webserver.listen(port, () => {
