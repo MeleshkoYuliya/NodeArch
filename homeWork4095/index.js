d731873cf7ddb836c77230a9f5613bc251305b62
MeleshkoYuliya <Yuliya.Meleshko@ventionteams.com>
2024-10-24 18:44:48 +0300

    Fixed styles

--- homeWork4095/index.js
+++ homeWork4095/index.js
@@ -3,36 +3,23 @@ const fs = require("fs");
 const path = require("path");
 const os = require("os");
 const webserver = express();
+const { v4: uuidv4 } = require("uuid");
+
+webserver.use(express.json());
 
 webserver.use(express.urlencoded({ extended: true }));
 
 const port = 7980;
-const logFN = path.join(__dirname, "_server.log");
-const logVotesFN = path.join(__dirname, "_data.log");
-const colors = [
-  { label: "Green", id: "green" },
-  { label: "Yellow", id: "yellow" },
-  { label: "Blue", id: "blue" },
-  { label: "Red", id: "red" },
-  { label: "Pink", id: "pink" },
-];
+const logDataFN = path.join(__dirname, "_data.log");
 
-function logLineSync(logFilePath, logLine) {
-  const logDT = new Date();
-  const time = logDT.toLocaleDateString() + " " + logDT.toLocaleTimeString();
-  const fullLogLine = time + " " + logLine;
-  const logFd = fs.openSync(logFilePath, "a+");
-  fs.writeSync(logFd, fullLogLine + os.EOL);
-  fs.closeSync(logFd);
-}
+webserver.use("/", express.static(path.resolve(__dirname)));
 
-function logVotesSync(logFilePath, color) {
+function saveDataSync(logFilePath, config) {
   const logFd = fs.openSync(logFilePath, "a+");
   const data = fs.readFileSync(logFilePath, { encoding: "utf8", flag: "r" });
 
-  let newData = data ? JSON.parse(data) : {};
-  const savedCount = newData[color] || 0;
-  newData = { ...newData, [color]: savedCount + 1 };
+  let newData = data ? JSON.parse(data) : [];
+  newData = [...newData, { ...config, id: uuidv4() }];
 
   fs.closeSync(logFd);
 
@@ -45,77 +32,70 @@ function logVotesSync(logFilePath, color) {
   fs.closeSync(logFdToWrite);
 }
 
-webserver.get("/variants", (req, res) => {
-  res.status(200).send(colors);
+webserver.post("/save-config", (req, res) => {
+  saveDataSync(logDataFN, req.body);
+  res.send("Request config saved!");
 });
 
-webserver.post("/vote", (req, res) => {
-  logVotesSync(logVotesFN, req.body.color);
-  res.send(`Voted, color: ${req.body.color}!`);
-});
-
-webserver.use(
-  "/voting",
-  express.static(path.resolve(__dirname))
-);
-
-
-webserver.post("/stat", (req, res) => {
-  const logFd = fs.openSync(logVotesFN, "a+");
-  const data = fs.readFileSync(logVotesFN, { encoding: "utf8", flag: "r" });
+webserver.get("/requests", (req, res) => {
+  const logFd = fs.openSync(logDataFN, "a+");
+  const data = fs.readFileSync(logDataFN, { encoding: "utf8", flag: "r" });
   fs.closeSync(logFd);
-  const newData = data ? JSON.parse(data) : {};
-
-  res.setHeader("Access-Control-Allow-Origin", "*");
-
-  const clientAccept = req.headers.accept;
-  if (clientAccept === "application/json") {
-    res.setHeader("Content-Type", "application/json");
-    res.send(newData);
-    return;
-  }
-
-  if (clientAccept === "application/xml") {
-    res.setHeader("Content-Type", "application/xml");
-    const colorsArr = Object.entries(newData);
-
-    const dataXML = `
-      <div>
-        ${colorsArr.map(
-          ([color, count]) => `
-            <div>
-              <span>${color}</span>:
-              <span>${count}</span>
-            </div>
-          `
-        )}
-      </div>
-    `;
-    res.send(dataXML);
-    return;
-  }
-
-  if (clientAccept === "text/html") {
-    res.setHeader("Content-Type", "text/html");
-    const colorsArr = Object.entries(newData);
-
-    const counts = colorsArr.map(
-      ([color, count]) => `<span>${color}</span>:<span>${count}</span>`
-    );
-
-    const dataHTML = `
-      <div>
-        ${counts}
-      </div>
-    `;
-    res.send(dataHTML);
-    return;
-  }
-
-  res.setHeader("Content-Type", "text/plain");
-  res.send(newData);
+  const newData = data ? JSON.parse(data) : [];
+  res.status(200).send(newData);
 });
+
+// webserver.post("/stat", (req, res) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+
+//   const clientAccept = req.headers.accept;
+//   if (clientAccept === "application/json") {
+//     res.setHeader("Content-Type", "application/json");
+//     res.send(newData);
+//     return;
+//   }
+
+//   if (clientAccept === "application/xml") {
+//     res.setHeader("Content-Type", "application/xml");
+//     const colorsArr = Object.entries(newData);
+
+//     const dataXML = `
+//       <div>
+//         ${colorsArr.map(
+//           ([color, count]) => `
+//             <div>
+//               <span>${color}</span>:
+//               <span>${count}</span>
+//             </div>
+//           `
+//         )}
+//       </div>
+//     `;
+//     res.send(dataXML);
+//     return;
+//   }
+
+//   if (clientAccept === "text/html") {
+//     res.setHeader("Content-Type", "text/html");
+//     const colorsArr = Object.entries(newData);
+
+//     const counts = colorsArr.map(
+//       ([color, count]) => `<span>${color}</span>:<span>${count}</span>`
+//     );
+
+//     const dataHTML = `
+//       <div>
+//         ${counts}
+//       </div>
+//     `;
+//     res.send(dataHTML);
+//     return;
+//   }
+
+//   res.setHeader("Content-Type", "text/plain");
+//   res.send(newData);
+// });
 
 webserver.listen(port, () => {
-  logLineSync(logFN, "web server running on port " + port);
+  console.log("web server running on port " + port);
 });
