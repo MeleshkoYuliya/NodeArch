2d87f52697ee383370285a112b3d9232daf0580f
MeleshkoYuliya <Yuliya.Meleshko@ventionteams.com>
2024-10-26 08:13:10 +0300

    resp html

--- homeWork4095/index.js
+++ homeWork4095/index.js
@@ -54,7 +54,10 @@ webserver.post("/send-request", async (req, res) => {
 
     const options = {
       method: req.body.method,
-      headers: req.body.headers,
+      headers: {
+        ...req.body.headers,
+        redirect: "error",
+      },
     };
 
     if (req.body.method !== "GET") {
@@ -68,10 +71,26 @@ webserver.post("/send-request", async (req, res) => {
     const url = params ? `${req.body.url}?${params}` : req.body.url;
 
     const response = await fetch(url, { ...options });
-    const json = await response.json();
+
     const headers = [...response.headers];
-    res.send({ json, headers, status: response.status });
+    const contentTypeData = headers.find(
+      ([name, value]) => name === "content-type"
+    );
+    const [contentTypeName, contentTypeValue] = contentTypeData;
+
+    if (contentTypeValue === "application/json") {
+      const json = await response.json();
+      res.send({ json, headers, status: response.status });
+    }
+
+    if (contentTypeValue.includes("text/html")) {
+      const json = await response.text();
+      res.setHeader("Content-Type", "text/html");
+      res.send({ json, headers, status: response.status });
+    }
   } catch (err) {
+    console.log(err.response);
+    res.send({ status: 400 });
     console.error(`Fetch problem: ${err.message}`);
   }
 });
