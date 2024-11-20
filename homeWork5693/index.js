7845c5fceda0db8d288d7f9b3fde3cd07812be37
MeleshkoYuliya <Yuliya.Meleshko@ventionteams.com>
2024-11-20 18:58:11 +0300

    Used repl

--- homeWork5693/index.js
+++ homeWork5693/index.js
@@ -6,7 +6,26 @@ const { createReadStream, createWriteStream } = require("node:fs");
 const { promisify } = require("node:util");
 const pipe = promisify(pipeline);
 
-const [, , arg] = process.argv;
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  prompt: "Укажите путь к файлу> ",
+});
+
+rl.prompt();
+rl.on("line", async (val) => {
+  if (!val) rl.close();
+  else {
+    await print(val);
+    rl.prompt();
+  }
+});
+
+rl.on("close", () => {
+  process.exit(0);
+});
 
 async function do_gzip(input, output) {
   const gzip = createGzip();
@@ -16,52 +35,59 @@ async function do_gzip(input, output) {
 }
 
 async function print(path) {
-  const files = await fs.promises.readdir(path, { withFileTypes: true });
+  try {
+    const files = await fs.promises.readdir(path, { withFileTypes: true });
 
-  console.log("--------------------------------------------------------------");
-  console.log(`СКАНИРУЕТСЯ ПАПКА: ${path}`);
-  console.log("--------------------------------------------------------------");
+    console.log(
+      "--------------------------------------------------------------"
+    );
+    console.log(`СКАНИРУЕТСЯ ПАПКА: ${path}`);
+    console.log(
+      "--------------------------------------------------------------"
+    );
 
-  for (const file of files) {
-    let createMessage = `Создается архив: ${file.name}.gz по пути ${file.path}/${file.name}.gz`;
+    for (const file of files) {
+      let createMessage = `Создается архив: ${file.name}.gz по пути ${file.path}/${file.name}.gz`;
 
-    let gzFile = files.find((gz) => gz.name === `${file.name}.gz`);
-    if (gzFile) {
-      const statFile = fs.statSync(`${file.path}/${file.name}`);
-      const fileModDTstatFile = new Date(statFile.mtime);
+      let gzFile = files.find((gz) => gz.name === `${file.name}.gz`);
+      if (gzFile) {
+        const statFile = fs.statSync(`${file.path}/${file.name}`);
+        const fileModDTstatFile = new Date(statFile.mtime);
 
-      const statGzFile = fs.statSync(`${file.path}/${gzFile.name}`);
-      const fileModDTstatGzFile = new Date(statGzFile.birthtimeMs);
+        const statGzFile = fs.statSync(`${file.path}/${gzFile.name}`);
+        const fileModDTstatGzFile = new Date(statGzFile.birthtimeMs);
 
-      if (fileModDTstatFile > fileModDTstatGzFile) {
-        createMessage = `Обновляется архив: ${file.name}.gz по пути ${file.path}/${file.name}.gz`;
+        if (fileModDTstatFile > fileModDTstatGzFile) {
+          createMessage = `Обновляется архив: ${file.name}.gz по пути ${file.path}/${file.name}.gz`;
 
-        fs.unlinkSync(`${file.path}/${gzFile.name}`);
-        gzFile = null;
+          fs.unlinkSync(`${file.path}/${gzFile.name}`);
+          gzFile = null;
+        }
+      }
+
+      if (
+        !file.isDirectory() &&
+        !file.name.startsWith(".") &&
+        !gzFile &&
+        !file.name.includes(".gz")
+      ) {
+        console.log(createMessage);
+        await do_gzip(
+          `${file.path}/${file.name}`,
+          `${file.path}/${file.name}.gz`
+        );
+        console.log(
+          `Архив: ${file.name}.gz по пути ${file.path}/${file.name}.gz Готов!`
+        );
+        console.log("              ");
+      }
+
+      if (file.isDirectory()) {
+        await print(`${file.path}/${file.name}`);
       }
     }
-
-    if (
-      !file.isDirectory() &&
-      !file.name.startsWith(".") &&
-      !gzFile &&
-      !file.name.includes(".gz")
-    ) {
-      console.log(createMessage);
-      await do_gzip(
-        `${file.path}/${file.name}`,
-        `${file.path}/${file.name}.gz`
-      );
-      console.log(
-        `Архив: ${file.name}.gz по пути ${file.path}/${file.name}.gz Готов!`
-      );
-      console.log("              ");
-    }
-
-    if (file.isDirectory()) {
-      await print(`${file.path}/${file.name}`, false);
-    }
+  } catch (err) {
+    console.log("Wrong path!");
+    rl.prompt();
   }
 }
-
-print(arg);
